654e17e0454b6508448bdb7b5b2bb78ba05560c6
Coria_32 <coriayo32@gmail.com>
2025-09-30 20:33:44 -0600

    huesped model y factory

--- app/Models/huesped.php
+++ app/Models/huesped.php
@@ -9,4 +9,44 @@ class huesped extends Model
 {
     /** @use HasFactory<\Database\Factories\HuespedFactory> */
     use HasFactory;
+    protected $table = 'huespedes';
+    protected $primaryKey = 'id_huesped';
+
+    protected $fillable = [
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'documento_identidad',
+        'tipo_documento',
+        'telefono',
+        'email',
+        'fecha_nacimiento',
+        'direccion',
+        'ciudad',
+        'pais'
+    ];
+
+    protected $casts = [
+        'fecha_nacimiento' => 'date'
+
+    ];
+
+    //relaciones
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class, 'id_huesped', 'id_huesped');
+    }
+
+    // Relación indirecta a través de Reserva
+    public function pagos()
+    {
+        return $this->hasManyThrough(
+            Pago::class,      // Modelo final
+            Reserva::class,   // Modelo intermedio
+            'id_huesped',     // FK en Reserva que apunta a Huesped
+            'id_reserva',     // FK en Pago que apunta a Reserva
+            'id_huesped',     // PK local en Huesped
+            'id_reserva'      // PK local en Reserva
+        );
+    }
 }
--- database/factories/HuespedFactory.php
+++ database/factories/HuespedFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Huesped;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\huesped>
- */
 class HuespedFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Huesped::class;
+
     public function definition(): array
     {
         return [
-            //
+            'nombre' => $this->faker->firstName(),
+            'apellido_paterno' => $this->faker->lastName(),
+            'apellido_materno' => $this->faker->optional()->lastName(),
+            'documento_identidad' => $this->faker->numerify('##########'),
+            'tipo_documento' => $this->faker->randomElement(['INE', 'Pasaporte', 'Cédula Profesional', 'Otro']),
+            'telefono' => $this->faker->optional()->numerify('55#######'),
+            'email' => $this->faker->optional()->unique()->safeEmail(),
+            'fecha_nacimiento' => $this->faker->date('Y-m-d', '-18 years'),
+            'direccion' => $this->faker->streetAddress(),
+            'ciudad' => $this->faker->city(),
+            'pais' => $this->faker->country(),
         ];
     }
 }

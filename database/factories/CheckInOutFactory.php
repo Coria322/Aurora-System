67ba3d0e907c67576f981de9b9619a01715a8704
Coria_32 <coriayo32@gmail.com>
2025-09-30 21:00:41 -0600

    check-in-out factory y model

--- database/factories/CheckInOutFactory.php
+++ database/factories/CheckInOutFactory.php
@@ -2,22 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\check_in_out;
+use App\Models\CheckInOut;
+use App\Models\Reserva;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\check_in_out>
- */
 class CheckInOutFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = check_in_out::class;
+
     public function definition(): array
     {
+        $fecha_check_in = $this->faker->dateTimeBetween('+1 days', '+1 month');
+        $fecha_check_out = (clone $fecha_check_in)->modify('+'.rand(1,10).' days');
+
         return [
-            //
+            'id_reserva' => Reserva::factory(),
+            'id_usuario' => User::factory(),
+            'fecha_check_in' => $fecha_check_in,
+            'fecha_check_out' => $fecha_check_out,
+            'observaciones_check_in' => $this->faker->optional()->sentence(8),
+            'observaciones_check_out' => $this->faker->optional()->sentence(8),
+            'estado_check' => $this->faker->randomElement(['check-in', 'check-out']),
         ];
     }
 }

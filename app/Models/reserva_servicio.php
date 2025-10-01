19fb5ba65fbc7630cc53803b480f5df0994ee2cb
Coria_32 <coriayo32@gmail.com>
2025-09-30 21:01:41 -0600

    reservaservicio model y factory

--- app/Models/reserva_servicio.php
+++ app/Models/reserva_servicio.php
@@ -7,6 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class reserva_servicio extends Model
 {
-    /** @use HasFactory<\Database\Factories\ReservaServicioFactory> */
     use HasFactory;
+
+    protected $table = 'reservas_servicios';
+    protected $primaryKey = 'id_reserva_servicio';
+
+    protected $fillable = [
+        'id_reserva',
+        'id_servicio',
+        'cantidad',
+        'precio_unitario',
+        'subtotal',
+        'fecha_servicio',
+    ];
+
+    protected $casts = [
+        'precio_unitario' => 'decimal:2',
+        'subtotal' => 'decimal:2',
+        'fecha_servicio' => 'date',
+    ];
+
+    // Relaciones
+    public function reserva()
+    {
+        return $this->belongsTo(Reserva::class, 'id_reserva', 'id_reserva');
+    }
+
+    public function servicio()
+    {
+        return $this->belongsTo(servicio::class, 'id_servicio', 'id_servicio');
+    }
 }
--- database/factories/ReservaServicioFactory.php
+++ database/factories/ReservaServicioFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Reserva;
+use App\Models\servicio;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\reserva_servicio>
- */
 class ReservaServicioFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = servicio::class;
+
     public function definition(): array
     {
+        $cantidad = $this->faker->numberBetween(1, 5);
+        $precio_unitario = $this->faker->randomFloat(2, 100, 2000);
+        $subtotal = $cantidad * $precio_unitario;
+
         return [
-            //
+            'id_reserva' => Reserva::factory(),
+            'id_servicio' => servicio::factory(),
+            'cantidad' => $cantidad,
+            'precio_unitario' => $precio_unitario,
+            'subtotal' => $subtotal,
+            'fecha_servicio' => $this->faker->dateTimeBetween('+1 days', '+2 months'),
         ];
     }
 }

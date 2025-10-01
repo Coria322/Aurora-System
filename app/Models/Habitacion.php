e0837dd43fdf27de37f13e867db37abf511b42c1
Coria_32 <coriayo32@gmail.com>
2025-09-30 20:40:26 -0600

    habitaciones model y factory

--- app/Models/Habitacion.php
+++ app/Models/Habitacion.php
@@ -7,6 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Habitacion extends Model
 {
-    /** @use HasFactory<\Database\Factories\HabitacionFactory> */
     use HasFactory;
+
+    protected $table = 'habitaciones';
+    protected $primaryKey = 'id_habitacion';
+
+    protected $fillable = [
+        'numero_habitacion',
+        'id_tipo_habitacion',
+        'estado',
+        'piso',
+        'descripcion',
+    ];
+
+    protected $casts = [
+        'estado' => 'string',
+    ];
+
+    // Relaciones
+    public function tipoHabitacion()
+    {
+        return $this->belongsTo(TipoHabitacion::class, 'id_tipo_habitacion', 'id_tipo_habitacion');
+    }
+
+    public function detalleReservas()
+    {
+        return $this->hasMany(DetalleReserva::class, 'id_habitacion', 'id_habitacion');
+    }
 }

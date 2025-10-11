1147ea42534d72bf4b09ab65bd610938c94bfed5
Coria_32 <coriayo32@gmail.com>
2025-10-10 18:14:43 -0600

    Se agregan scopes para ver los tipos de habitación activos, e inactivos

--- app/Models/tipoHabitacion.php
+++ app/Models/tipoHabitacion.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -34,5 +36,20 @@ class TipoHabitacion extends Model
     public function habitaciones()
     {
         return $this->hasMany(Habitacion::class, 'id_tipo_habitacion', 'id_tipo_habitacion');
+    }
+
+    #[Scope]
+    /**
+     * Summary of Activas
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return void
+     */
+    protected function Activas(Builder $query){
+        $query -> where('activo', true);
+    }
+
+    #[Scope] 
+    protected function Inactivas(Builder $query){
+        $query -> whereNot('activo', true);
     }
 }

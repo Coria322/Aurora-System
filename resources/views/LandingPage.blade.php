d5938d1dd8f217e59062ff76d50989892c2a2b55
Coria_32 <coriayo32@gmail.com>
2025-10-12 17:24:07 -0600

    agregando parciales y estilos del landing

--- resources/views/LandingPage.blade.php
+++ resources/views/LandingPage.blade.php
@@ -10,6 +10,15 @@
     @include('partials.navbar')
 </header>
 <body>
-    @include('partials.vidBanner')
+    <section id="inicio">
+        @include('partials.vidBanner')
+    </section>
+    <section id="habitaciones">
+        <h1 class="habitaciones__title">
+            Vive el lujo de Hospedarte aquí
+        </h1>
+        @include('partials.carousel-element')
+    </section>
+    
 </body>
 </html>
--- resources/views/partials/carousel-element.blade.php
+++ resources/views/partials/carousel-element.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+<div class="habitacion__card">
+    @if ($tipoHab -> imagen)
+        <img src="{{ asset('images/' . $tipoHab -> imagen) }}" alt="Imagen de {{ $tipoHab -> nombre }}">
+    @else
+        <img src="{{ asset('images/Habitaciones/habitacion_generica.png') }}" alt="imagen por defecto">
+    @endif
 </div>

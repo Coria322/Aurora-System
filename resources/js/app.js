3ef127cfb4abee448b7a0b3516d9e60fbf390f5c
Coria_32 <coriayo32@gmail.com>
2025-10-11 19:19:48 -0600

    Scripts para controlar los videos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+import '../js/bannervid';
+
 document.addEventListener('DOMContentLoaded', function () {
     const navbarToggle = document.getElementById('navbarToggle');
     const navbarMenu = document.getElementById('navbarMenu');

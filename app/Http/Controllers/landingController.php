2bdc8f6445119442a70ffc810cb36149fd060247
Coria_32 <coriayo32@gmail.com>
2025-10-12 17:23:44 -0600

    agregando controlador del landing page

--- app/Http/Controllers/landingController.php
+++ app/Http/Controllers/landingController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TipoHabitacion;
 use Illuminate\Http\Request;
 
 class landingController extends Controller
 {
     public function showHome(){
+        $tipoHab = TipoHabitacion::listar()->get();
+
         return view('LandingPage');
     }
 }

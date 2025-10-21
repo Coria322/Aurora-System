375ca84f6f8635a2eac5dd380f5a0fb156e90555
Coria_32 <coriayo32@gmail.com>
2025-10-21 15:12:28 -0600

    Middlewares para redireccionar y verificar por tipo de usuario

---
+++ app/Http/Middleware/redirectType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class redirectType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        
+        if(!$user->tipo_usuario){
+            abort(406, "Tu cuenta tiene problemas, Comunicate con soporte");
+        }
+
+        $ruta = match ($user->tipo_usuario){
+            'admin' => 'admin.dashboard',
+            'cliente' => 'dashboard',
+            'empleado' => 'empleado.dashboard',
+            default => 'home'
+        };
+
+        if(!$request -> routeIs($ruta)){
+            return redirect($ruta);
+        }
+
+        return $next($request);
+    }
+}
